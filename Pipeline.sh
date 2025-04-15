0bf03934c161ae2452552a8772b407f3a02f9155
conveytopraveenkumar <92304904+conveytopraveenkumar@users.noreply.github.com>
2025-04-15 14:09:04 +0530

    Update Pipeline.sh

--- Pipeline.sh
+++ Pipeline.sh
@@ -5,12 +5,12 @@
  echo "Running pipeline..."
  
  # Create a directory for the output
- mkdir -p /home/ec2-user/output
+ mkdir -p /home/ubuntu/output
  
  # Simulate work by creating a file
- echo "Pipeline output at $(date)" > /home/ec2-user/output/result.txt
+ echo "Pipeline output at $(date)" > /home/ubuntu/output/result.txt
  
  # Upload the output to S3
- aws s3 cp /home/ec2-user/output/result.txt s3://$S3_BUCKET_NAME/result-$(date +%s).txt
+ aws s3 cp /home/ubuntu/output/result.txt s3://$S3_BUCKET_NAME/result-$(date +%s).txt
  
  echo "Artifact uploaded to S3."
