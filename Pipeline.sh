1d491a30f81a957ab179bced53f20ce586c7d6af
conveytopraveenkumar <92304904+conveytopraveenkumar@users.noreply.github.com>
2025-04-16 14:51:26 +0530

    Update Pipeline.sh

--- Pipeline.sh
+++ Pipeline.sh
@@ -20,6 +20,7 @@ aws sts get-caller-identity || { echo "AWS credentials not available! Exiting...
 echo "Copying file from EC2 to S3 bucket..."
 
 # Upload with debug mode
-aws s3 cp result.txt s3://cicd-github-artifacts-bucket/result-$(date +%s).txt --debug || { echo "S3 upload failed! Exiting..."; exit 1; }
+timestamp=$(date +%s)
+aws s3 cp result.txt s3://cicd-github-artifacts-bucket/result-$timestamp.txt --debug
 
 echo "Artifact uploaded to S3."
