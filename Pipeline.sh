70a80e165257474fc3e20b069cad7f46f209c82b
conveytopraveenkumar <92304904+conveytopraveenkumar@users.noreply.github.com>
2025-04-16 10:08:11 +0530

    Update Pipeline.sh

--- Pipeline.sh
+++ Pipeline.sh
@@ -10,7 +10,15 @@
  # Simulate work by creating a file
  echo "Pipeline output at $(date)" > /home/ubuntu/output/result.txt
  
+ cd /home/ubuntu/output
+ chmod +r /home/ubuntu/output/result.txt
+ ls -lart
+ pwd
+ sudo apt update
+ sudo apt install awscli
+ aws --version
+ echo "copying file from EC2 to S3 bucket
  # Upload the output to S3
- aws s3 cp /home/ubuntu/output/result.txt s3://$S3_BUCKET_NAME/result-$(date +%s).txt
+ aws s3 cp /home/ubuntu/output/result.txt s3://cicd-github-artifacts-bucket/result-$(date +%s).txt
  
  echo "Artifact uploaded to S3."
